9904cf0fd082f33c7ca2821e81f692fa2016393e
arti763 <artur76321@gmail.com>
2025-10-28 22:42:00 +0100

    Edycja Profilu

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 # accounts/urls.py
 
 from django.urls import path, include # Upewnij się, że 'include' jest zaimportowane
-from .views import RegisterView
+from .views import RegisterView, profile_edit_view
 
 urlpatterns = [
     # Ścieżka do widoku rejestracji
     path('register/', RegisterView.as_view(), name='register'),
-
+    path('profile/', profile_edit_view, name='profile_edit'), # NOWA ŚCIEŻKA
     # Dołączenie wszystkich wbudowanych URLi Django dla uwierzytelniania
     # To automatycznie doda ścieżki takie jak:
     # login/, logout/, password_change/, password_reset/ itd.
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,27 @@
+# accounts/views.py
+
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect # Dodaj importy
+from django.contrib.auth.decorators import login_required # Dodaj import
+from django.contrib import messages # Dodaj import
+from .forms import CustomUserCreationForm, UserProfileForm # Dodaj import UserProfileForm
 
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+# NOWY WIDOK DO EDYCJI PROFILU
+@login_required # Wymaga zalogowania
+def profile_edit_view(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Twój profil został zaktualizowany!')
+            return redirect('profile_edit') # Przekieruj z powrotem na tę samą stronę
+    else:
+        form = UserProfileForm(instance=request.user) # Wypełnij formularz danymi użytkownika
+
+    return render(request, 'registration/profile_edit.html', {'form': form})
